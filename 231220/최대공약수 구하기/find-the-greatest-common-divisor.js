93f803a43558ba092c76515e3f88176997a39143
BoYoung Yun <87300419+BoyoungYun@users.noreply.github.com>
2023-12-20 21:27:38 +0900

    Title: 최대공약수 구하기, Time: 227ms, Memory: 13MB, Status: Passed - CodeTree

--- 231220/최대공약수 구하기/find-the-greatest-common-divisor.js
+++ 231220/최대공약수 구하기/find-the-greatest-common-divisor.js
@@ -1,6 +1,6 @@
 function printGCD(n, m) {
     let answer = 0;
-    for(let i=2; i<=m; i++) {
+    for(let i=1; i<=m; i++) {
         if(n%i === 0 && m%i === 0) {
             answer = i;
         }
