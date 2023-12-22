0257d5f695691bd2f558f251c6c7eb85e8f8b067
BoYoung Yun <87300419+BoyoungYun@users.noreply.github.com>
2023-12-22 16:31:01 +0900

    Title: 1부터 특정 수까지의 합, Time: 246ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 231222/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -0,0 +1,12 @@
+function divideTen(N) {
+    let sum = 0;
+    
+    for(let i=1; i<=N; i++) {
+        sum += i;
+    }
+    return sum / 10;
+}
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+console.log(divideTen(input))
